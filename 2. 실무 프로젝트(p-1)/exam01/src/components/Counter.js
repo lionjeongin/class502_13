afd575b348e06d6bbc92e281d71f1eb8c78f744c
lionjeongin <cindy93489163@gmail.com>
2024-04-04 17:53:52 +0900

    웹브라우저객체 & ref

--- 2. 실무 프로젝트(p-1)/exam01/src/components/Counter.js
+++ 2. 실무 프로젝트(p-1)/exam01/src/components/Counter.js
@@ -1,16 +1,18 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const Counter = () => {
-  let num = 10;
+  let num = useRef(10);
+
   console.log('렌더링!', 'num', num);
 
-  const [number, setNumber] = useState(0); // 비구조화 할당 / 함수형 컴포넌트
+  const [number, setNumber] = useState(0);
 
   const plus = () => {
-    num++; //매번 렌더링이 다시 되기 때문에(매번 함수가 호출되기 때문에 초기화 돼서) 증가 안된다. 지역 변수 못 쓴다.
+    num.current++; // 11
     setNumber(number + 1);
   };
   const minus = () => setNumber(number - 1);
+
   return (
     <>
       <h1>{number}</h1>
@@ -24,4 +26,4 @@ const Counter = () => {
   );
 };
 
-export default Counter; // 모듈 내보내기
+export default Counter;
