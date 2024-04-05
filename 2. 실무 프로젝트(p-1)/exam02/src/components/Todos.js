f7480d011904449c32970b3c3a111e768ec886e7
lionjeongin <cindy93489163@gmail.com>
2024-04-05 17:57:12 +0900

    컴포넌트 & Hooks

--- 2. 실무 프로젝트(p-1)/exam02/src/components/Todos.js
+++ 2. 실무 프로젝트(p-1)/exam02/src/components/Todos.js
@@ -1,21 +1,28 @@
+import { useState } from 'react';
 import {
   MdOutlineCheckBox,
   MdOutlineCheckBoxOutlineBlank,
 } from 'react-icons/md';
 
-const items = [
+const initialValue = [
   { id: 1, title: '할일1', done: false },
   { id: 2, title: '할일2', done: true },
   { id: 3, title: '할일3', done: false },
 ];
 
 const Todos = () => {
-  //const listItems = items.map((item) => <li>{item.title}</li>);
+  const [items, setItems] = useState(initialValue);
+
+  const onToggle = (id) => {
+    const newItems = items.map(item => item.id === id ? {...item, done: !item.done} : item);
+
+    setItems(newItems);
+  };
 
   return (
     <ul>
       {items.map(({ id, title, done }) => (
-        <li key={id}>
+        <li key={id} onClick={() => onToggle(id)}>
           {done ? <MdOutlineCheckBox /> : <MdOutlineCheckBoxOutlineBlank />}
           {title}
         </li>
