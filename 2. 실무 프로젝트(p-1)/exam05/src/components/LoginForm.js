4a3900a8970676381f41ccf4b2d51ef30faa1fe7
lionjeongin <cindy93489163@gmail.com>
2024-04-09 11:14:46 +0900

    리액트 로그인 페이지

--- 2. 실무 프로젝트(p-1)/exam05/src/components/LoginForm.js
+++ 2. 실무 프로젝트(p-1)/exam05/src/components/LoginForm.js
@@ -1,16 +1,36 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { BigButton } from './commons/ButtonStyle';
+
+const commonStyle = css`
+  width: 100%;
+`;
+
+const OuterBox = styled.div`
+  ${commonStyle}
+  position: fixed;
+  height: 100%;
+  left: 0;
+  top: 0;
+  display: flex;
+  align-items: center;
+`;
 
 const FormBox = styled.form`
   width: 420px;
   margin: 0 auto;
+
+  h1 {
+    text-align: center;
+  }
 `;
 
 const InputBox = styled.input`
+  ${commonStyle}
   display: block;
   height: 45px;
   border: 1px solid #ccc;
   border-radius: 5px;
-  width: 100%;
+  padding: 0 10px;
 
   & + & {
     margin-top: 5px;
@@ -19,14 +39,17 @@ const InputBox = styled.input`
 
 const LoginForm = () => {
   return (
-    <>
-      <h1>로그인</h1>
+    <OuterBox>
       <FormBox autoComplete="off">
+        <h1>로그인</h1>
+
         <InputBox type="text" placeholder="아이디" />
         <InputBox type="password" placeholder="비밀번호" />
-        <button type="submit">로그인</button>
+        <BigButton type="submit" bgcolor="blue">
+          로그인
+        </BigButton>
       </FormBox>
-    </>
+    </OuterBox>
   );
 };
 
