9158b99f881ee8b040c12b0766c3a53ed4477218
lionjeongin <cindy93489163@gmail.com>
2024-04-16 16:29:38 +0900

    상속

---
+++ 4. 프로그래밍 언어활용/source/day02/src/exam02/Schedule2.java
@@ -0,0 +1,15 @@
+package exam02;
+
+public class Schedule2 {
+    private int year;
+    private int month;
+    private int day;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
