d411de29621382ab416447343bf29026d52dd5bc
lionjeongin <cindy93489163@gmail.com>
2024-04-17 15:51:15 +0900

    중간고사 풀이 2

--- 3. 응용 SW 기초 기술활용/source/day02/src/exam01/practice/Re06.java
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/practice/Re06.java
@@ -5,6 +5,19 @@ import java.util.Arrays;
 public class Re06 {
     // 거꾸로 배열
     public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
 
+        // 거꾸로 배열
+        int[] reverseArr = new int[5];
+
+        // 원본배열(arr)의 마지막 인덱스(int i = arr.length - 1)부터 탐색하여
+        // 거꾸로 배열(reverseArr)에 순서대로 값을 세팅한다.
+        for (int i = arr.length - 1, j = 0; i >= 0; i--, j++) {
+            reverseArr[j] = arr[i];
+        }
+
+        // 원본 배열과 거꾸로 배열의 값을 출력한다.
+        System.out.println("arr : " + Arrays.toString(arr));
+        System.out.println("reverseArr : " + Arrays.toString(reverseArr));
     }
 }
