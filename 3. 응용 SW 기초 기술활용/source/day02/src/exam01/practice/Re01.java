c859a2e06f28488258641a080208b0aaa46bc3b9
lionjeongin <cindy93489163@gmail.com>
2024-04-17 15:48:31 +0900

    연습문제 풀이

--- 3. 응용 SW 기초 기술활용/source/day02/src/exam01/practice/Re01.java
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/practice/Re01.java
@@ -3,7 +3,13 @@ package exam01.practice;
 public class Re01 {
     //변수 두 개를 선언해서 20과 3.0을 대입하고 두 변수의 사칙연산 결과를 정수로 출력해 보시오
     public static void main(String[] args) {
+        int num1 = 20;
+        double num2 = 3.0;
 
+        int result = (int)(num1+num2); // int num1 -> double : 연산은 같은 자료형일 때 가능하다.
+        // int result = num1+(int)num2; int를 저렇게 쓰면 만약 num2가 3.4일 때 3이 돼서 오차범위 발생한다.
+
+        System.out.println(result); // 23
 
     }
 }
--- 3. 응용 SW 기초 기술활용/source/day02/src/exam01/practice/Re02.java
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/practice/Re02.java
@@ -7,5 +7,9 @@ public class Re02 {
     // (예 : 학생1, 방번호 1번)
     public static void main(String[] args) {
 
+            for (int i = 1; i <= 40; i++) {
+                int roomNo = i % 10; // 0 ~ 9
+                System.out.printf("학생%d, 방번호 %d번%n", i, roomNo);
+            }
     }
 }
