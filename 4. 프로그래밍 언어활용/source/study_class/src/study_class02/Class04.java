b0ff33425310516433553d347d5b945955f86bd6
lionjeongin <cindy93489163@gmail.com>
2024-04-18 15:16:32 +0900

    클래스

---
+++ 4. 프로그래밍 언어활용/source/study_class/src/study_class02/Class04.java
@@ -0,0 +1,13 @@
+package study_class02;
+
+public class Class04 {
+    class HandPhoneMainClass {
+        public static void main() {
+            //클래스로부터 객체 생성(instance)
+
+            HandPhone samsungHpc = new HandPhone();
+            HandPhone lgHpc = new HandPhone();
+            HandPhone motoHpc = new HandPhone();
+        } // end of main()
+    } // end of class클래스이름 참조변수 = new 클래스이름();
+}
