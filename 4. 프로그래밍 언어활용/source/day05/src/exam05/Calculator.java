0af807dcf323dd958104113bab36e2e5a2ec0ba4
lionjeongin <cindy93489163@gmail.com>
2024-04-19 17:14:39 +0900

    추상클래스

--- 4. 프로그래밍 언어활용/source/day05/src/exam05/Calculator.java
+++ 4. 프로그래밍 언어활용/source/day05/src/exam05/Calculator.java
@@ -1,6 +1,7 @@
 package exam05;
 
 public abstract class Calculator {
-    //public int add(int num1, int num2); // 추상 메서드
+    //public int add(int num1, int num2);  // 추상 메서드
+    int num = 10;
     public abstract int add(int num1, int num2);
 }
---
+++ 4. 프로그래밍 언어활용/source/day05/src/exam05/ComplexityCalculator.java
@@ -0,0 +1,2 @@
+package exam05;public class ComplexityCalculator {
+}
