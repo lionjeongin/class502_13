2e64a1b360bb53febc0fbebc88840bdefa6214d8
lionjeongin <cindy93489163@gmail.com>
2024-04-23 12:43:14 +0900

    try-catch

---
+++ 4. 프로그래밍 언어활용/source/day07/src/exam03/Ex01.java
@@ -0,0 +1,11 @@
+package exam03;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        try(Resources resources = new Resources()) {
+            // res가 AutoClosable 인터페이스 구현 객체인지 체크 -> close() 메서드 자동 호출
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
