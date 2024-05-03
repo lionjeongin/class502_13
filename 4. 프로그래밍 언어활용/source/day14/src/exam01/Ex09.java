e91cc95dbbf894e00db7892190cfd7706a834c76
lionjeongin <cindy93489163@gmail.com>
2024-05-03 12:20:00 +0900

    커밋

--- 4. 프로그래밍 언어활용/source/day14/src/exam01/Ex09.java
+++ 4. 프로그래밍 언어활용/source/day14/src/exam01/Ex09.java
@@ -2,10 +2,18 @@ package exam01;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public class Ex09 {
     public static void main(String[] args) {
         List<String> alpah = Arrays.asList("abc", "def", "ghi");
+        String[] upperAlah2 = alpah.stream().map(new Function<String, String>() {
+
+            @Override
+            public String apply(String s) {
+                return s.toUpperCase();
+            }
+        })
 
         // String[] upperAlpah = alpah.stream().map(s -> s.toUpperCase()).toArray(i -> new String[i]);
         //-> String[] upperAlpah = alpah.stream().map(String::toUpperCase).toArray(i -> new String[i]);
---
+++ 4. 프로그래밍 언어활용/source/day15/src/exam01/Ex10.java
@@ -0,0 +1,14 @@
+package exam01;
+
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        Book b = new Book(1000, "책1");
+        Optional<String> opt = Optional.ofNullable(null);
+        String title = opt.orElseGet(() -> b.getTitle());
+       // String title = opt.orElseGet(b::getTitle);
+    }
+}
