40278a888ea55ace41624a37185ab315796243eb
lionjeongin <cindy93489163@gmail.com>
2024-05-16 17:49:37 +0900

    자바 직렬화

--- 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex09.java
+++ 4. 프로그래밍 언어활용/source/day22/src/exam01/Ex09.java
@@ -1,6 +1,5 @@
 package exam01;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -12,14 +11,22 @@ public class Ex09 {
 
             /**
              * Book book = new Book(..., ..., ...);
-             * - 복구시에 필요한 데이터가 저장 -> 다시 객체 생성, 값 대임
+             * - 복구시에 필요한 데이터가 저장 -> 다시 객체 생성, 값 대입
              */
+
+            String str = (String)ois.readObject();
+            System.out.println(str);
+
             Book book1 = (Book)ois.readObject();
             System.out.println(book1);
 
+            Book book2 = (Book)ois.readObject();
+            System.out.println(book2);
+
+
+
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
-
         }
     }
 }
