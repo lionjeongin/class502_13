a0179f4d1a2bb3fd285dd0c9a570eb18636c27a3
lionjeongin <cindy93489163@gmail.com>
2024-05-17 16:43:46 +0900

    데몬 쓰레드

--- 4. 프로그래밍 언어활용/source/day23/src/exam01/Ex07.java
+++ 4. 프로그래밍 언어활용/source/day23/src/exam01/Ex07.java
@@ -6,6 +6,10 @@ public class Ex07 {
     public static void main(String[] args) {
         Thread th = new Thread(() -> {
             while (true) {
+                Thread.sleep(3000);
+
+                System.out.println("저장!");
+            }
         });
     }
 }
