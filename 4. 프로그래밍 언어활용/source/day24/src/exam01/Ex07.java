6958d19be122e776cb668c9ce3021a95815c022a
lionjeongin <cindy93489163@gmail.com>
2024-05-20 16:51:24 +0900

    suspend(), resume(), stop()

--- 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex07.java
+++ 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex07.java
@@ -1,26 +1,26 @@
 package exam01;
 
 public class Ex07 {
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args) throws InterruptedException {
         Thread th = new Thread(() -> {
             Thread th2 = Thread.currentThread();
             while(true) {
                 try {
                     System.out.println("isInterrupted : " + th2.isInterrupted());
-                    for (int j = 0; j < 100000000L; j++);
-                    Thread.sleep(10000); // WAITING - 일시 정지
-                    // interrupted() -> InterruptedExceptuon 발생, isInterrupted - false
+
+                    Thread.sleep(10000); // WAITING  - 일시 정지
+                    // interrupted() -> InterruptedException 발생, isInterrupted - false
                 } catch (InterruptedException e) { // RUNNABLE - 실행 대기
                     System.out.println("InterruptedException 발생!");
+                    System.out.println(th2.isInterrupted());// false
                     th2.interrupt(); // InterruptedException
+
                 }
             }
         });
 
         th.start();
-
         Thread.sleep(3000);
-
         th.interrupt(); // InterruptedException 발생, isInterrupted -> true
     }
 }
