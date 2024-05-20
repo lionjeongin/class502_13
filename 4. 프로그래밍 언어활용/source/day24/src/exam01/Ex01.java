3fd158248b3e97e34d850876d372928b1d5c355b
lionjeongin <cindy93489163@gmail.com>
2024-05-20 14:18:18 +0900

    쓰레드 start() 호출

--- 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex01.java
@@ -2,8 +2,9 @@ package exam01;
 
 public class Ex01 {
     public static void main(String[] args) {
-        Thread th = new Thread();
-        th.run();
+        Ex01_1 th = new Ex01_1();
+        //th.run();
+        th.start();
 
         for (int i = 0; i < 5; i++) {
             System.out.println("메인 쓰레드 - " + i);
@@ -11,7 +12,7 @@ public class Ex01 {
     }
 }
 
-class EX01_1  extends Thread {
+class Ex01_1 extends Thread {
     public void run() {
         for (int i = 0; i < 5; i++) {
             System.out.println("쓰레드 - " + i);
