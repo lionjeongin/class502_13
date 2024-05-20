7e96af95fc822c3afed849e916e8c666d655d694
lionjeongin <cindy93489163@gmail.com>
2024-05-20 15:00:01 +0900

    쓰레드, 시분할방식

--- 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex03.java
+++ 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex03.java
@@ -4,6 +4,6 @@ public class Ex03 {
     public static void main(String[] args) {
 
         Thread th = new Thread(() -> System.out.println("실행부분..."));
-            
+
     }
 }
---
+++ 4. 프로그래밍 언어활용/source/day24/src/exam01/Ex04.java
@@ -0,0 +1,9 @@
+package exam01;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        Runnable r1 = () -> {
+            for (int i = 0; i < 300; i++);
+        };
+    }
+}
