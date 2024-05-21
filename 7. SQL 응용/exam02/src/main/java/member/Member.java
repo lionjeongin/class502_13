d2fe56f0ae4b24bec0a08cbfaac4cf58a02064ee
lionjeongin <cindy93489163@gmail.com>
2024-05-21 17:44:43 +0900

    maven

---
+++ 7. SQL 응용/exam02/src/main/java/member/Member.java
@@ -0,0 +1,18 @@
+package member;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/*
+@EqualsAndHashCode
+@Getter @Setter @ToString */
+@Data
+@NoArgsConstructor // 기본 생성자
+@AllArgsConstructor
+public class Member {
+    private String userId;
+    private String userNm;
+    private String email;
+    private LocalDateTime regDt;
+}
