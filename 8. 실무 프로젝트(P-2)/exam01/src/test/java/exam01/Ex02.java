f84fd1c0687b9f5f443e2270e2bc3ac50886ab18
lionjeongin <cindy93489163@gmail.com>
2024-05-23 17:54:39 +0900

    깃올리기

--- 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex02.java
+++ 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex02.java
@@ -5,13 +5,15 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Ex02 {
     private String url = "jdbc:oracle:thin:@localhost:1521:XE";
     private String user = "STUDY";
     private String password = "oracle";
 
-    @BeforeAll // 모든 테스트 메서드 호출 전에 단 한 번 실행 - 공통 초기화
+    @BeforeAll // 모든 테스트 메서드 호출 전에 단한번 실행 - 공통 초기화
     static void init() {
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -19,17 +21,18 @@ public class Ex02 {
             e.printStackTrace();
         }
     }
+
     @Test
     void test1() {
         String keyword = "사용자";
         String sql = "SELECT * FROM MEMBER WHERE USER_NM LIKE ?";
-        try (Connection conn = DriverManager.getConnection(url, user, password);
+        try (Connection conn = DriverManager.getConnection(url, user,password);
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             pstmt.setString(1, "%" + keyword + "%");
 
             ResultSet rs = pstmt.executeQuery();
-
+            List<Member> members = new ArrayList<>();
             while(rs.next()) {
                 long userNo = rs.getLong("USER_NO");
                 String userId = rs.getString("USER_ID");
@@ -38,11 +41,23 @@ public class Ex02 {
                 String mobile = rs.getString("MOBILE");
                 LocalDateTime regDt = rs.getTimestamp("REG_DT").toLocalDateTime();
 
+                Member member = new Member();
+                /*
+                member.setUserNo(userNo);
+                member.setUserId(userId);
+                member.setUserPw(userPw);
+                member.setUserNm(userNm);
+                member.setMobile(mobile);
+                member.setRegDt(regDt);
+                */
+                members.add(member);
+                /*
                 System.out.printf("USER_NO:%d, USER_ID:%s, USER_PW:%s, USER_NM:%s, MOBILE:%s, REG_DT:%s%n", userNo, userId, userPw, userNm, mobile, regDt);
+
+                 */
             }
 
             rs.close();
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -50,14 +65,14 @@ public class Ex02 {
 
     @Test
     void test2() {
-        String userId = "USER04";
+        String userId = "USER101";
         String userPw = "123456";
-        String userNm = "사용04";
+        String userNm = "사용자101";
         String mobile = "01000000000";
+
         String sql = "INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM, MOBILE) VALUES (SEQ_MEMBER.NEXTVAL, ?, ?, ?, ?)";
         try (Connection conn = DriverManager.getConnection(url, user, password);
-            PreparedStatement pstmt = conn.prepareStatement(sql, new
-            String[] {"USER_NO"})) {
+             PreparedStatement pstmt = conn.prepareStatement(sql, new String[] {"USER_NO"})) {
 
             pstmt.setString(1, userId);
             pstmt.setString(2, userPw);
@@ -72,7 +87,6 @@ public class Ex02 {
                 long userNo = rs.getLong(1);
                 System.out.printf("USER_NO : %d%n", userNo);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
