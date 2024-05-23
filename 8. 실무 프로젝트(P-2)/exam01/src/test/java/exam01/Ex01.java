085401c76d8e97d18e96b4819722d1865f611abd
lionjeongin <cindy93489163@gmail.com>
2024-05-23 11:48:33 +0900

    SQL 주입의 가능성 높다.

--- 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex01.java
+++ 8. 실무 프로젝트(P-2)/exam01/src/test/java/exam01/Ex01.java
@@ -3,6 +3,7 @@ package exam01;
 import org.junit.jupiter.api.Test;
 
 import java.sql.*;
+import java.time.LocalDateTime;
 
 public class Ex01 {
     @Test
@@ -13,11 +14,13 @@ public class Ex01 {
             e.printStackTrace();
         }
 
+        // String url = "jdbc:oracle:thin:@localhost:1521:EE"
         String url = "jdbc:oracle:thin:@localhost:1521:XE";
-        String user = "STUDY";
-        String password = "oracle";
+        String user = "STUDY"; // SCOTT
+        String password = "oracle";  // tiger
+
         try (Connection conn = DriverManager.getConnection(url, user, password);
-            Statement stmt = conn.createStatement()) {
+             Statement stmt = conn.createStatement()) {
 
             String sql = "INSERT INTO MEMBER (USER_NO, USER_ID, USER_PW, USER_NM, MOBILE) VALUES (SEQ_MEMBER.NEXTVAL, 'USER02', '123456', '사용자02', '01000000000')";
 
@@ -28,6 +31,7 @@ public class Ex01 {
             e.printStackTrace();
         }
     }
+
     @Test
     void test2() {
         try {
@@ -38,14 +42,25 @@ public class Ex01 {
         }
 
         String url = "jdbc:oracle:thin:@localhost:1521:XE";
-        String user = "STUDY";
-        String password = "oracle";
+        String user = "STUDY"; // SCOTT
+        String password = "oracle"; // tiger
         try(Connection conn = DriverManager.getConnection(url, user, password);
             Statement stmt = conn.createStatement()) {
+            String keyword = "사용자"; // SQL 주입의 가능성 높다.
+            String sql = "SELECT * FROM MEMBER WHERE USER_NM LIKE '%" + keyword + "%'";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()) {
+                long userNo = rs.getLong("USER_NO");
+                String userId = rs.getString("USER_ID");
+                String userPw = rs.getString("USER_PW");
+                String userNm = rs.getString("USER_NM");
+                String mobile = rs.getString("MOBILE");
+                LocalDateTime regDt = rs.getTimestamp("REG_DT").toLocalDateTime();
 
-            String sql = "SELECT * FROM MEMBER";
-            ResultSet resultSet = stmt.executeQuery(sql);
+                System.out.printf("USER_NO:%d, USER_ID:%s, USER_PW:%s, USER_NM:%s, MOBILE:%s, REG_DT:%s%n", userNo, userId, userPw, userNm, mobile, regDt);
+            }
 
+            rs.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
