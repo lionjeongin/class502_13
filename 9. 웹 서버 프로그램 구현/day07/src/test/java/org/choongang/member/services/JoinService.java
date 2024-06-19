14df531c3b6c9abdd568ba50bd7bac6519471879
lionjeongin <cindy93489163@gmail.com>
2024-06-19 17:09:34 +0900

    day07

--- 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/services/JoinService.java
+++ 9. 웹 서버 프로그램 구현/day07/src/test/java/org/choongang/member/services/JoinService.java
@@ -5,6 +5,7 @@ import org.choongang.global.validators.Validator;
 import org.choongang.member.controllers.RequestJoin;
 import org.choongang.member.entities.Member;
 import org.choongang.member.mapper.MemberMapper;
+import org.mindrot.jbcrypt.BCrypt;
 
 // 회원 가입 기능
 public class JoinService {
@@ -12,7 +13,7 @@ public class JoinService {
     private Validator<RequestJoin> validator;
     private MemberMapper mapper;
 
-    public JoinService(Validator<RequestJoin> validator) {
+    public JoinService(Validator<RequestJoin> validator, MemberMapper mapper) {
         this.validator = validator;
         this.mapper = mapper;
     }
@@ -21,6 +22,19 @@ public class JoinService {
 
         // 유효성 검사
         validator.check(form);
+
+        // 비밀번호 해시화 - BCrypt
+        String hash = BCrypt.hashpw(form.getPassword(), BCrypt.gensalt(12));
+
+        Member member = new Member();
+        member.setEmail(form.getEmail());
+        member.setPassword(hash);
+        member.setUserName(form.getUserName());
+
+        int result = mapper.register(member);
+        if (result < 1) {
+            throw new BadRequestException("회원가입에 실패하였습니다.");
+        }
 
     }
 }
