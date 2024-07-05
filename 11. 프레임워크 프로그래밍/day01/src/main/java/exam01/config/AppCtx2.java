640ee940c7d7fb56f09f35574e9cc34f7df869f8
lionjeongin <cindy93489163@gmail.com>
2024-07-05 15:11:35 +0900

    스프링 DI 설정 및 사용

--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/config/AppCtx2.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/config/AppCtx2.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.format.DateTimeFormatter;
+
 @Configuration
 public class AppCtx2 {
 
@@ -40,5 +42,12 @@ public class AppCtx2 {
         InfoService infoService = new InfoService();
 
         return infoService;
+    }
+
+    @Bean // 자바 JDK, 외부 라이브러리는 수동 등록이 필수
+    public DateTimeFormatter dateTimeFormatter() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
+
+        return formatter;
     }
 }
--- 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/services/InfoService.java
+++ 11. 프레임워크 프로그래밍/day01/src/main/java/exam01/member/services/InfoService.java
@@ -5,11 +5,15 @@ import exam01.member.entities.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
 public class InfoService {
     private MemberDao memberDao;
+
+    private DateTimeFormatter formatter;
 
     @Autowired
     @Qualifier("mDao")
@@ -17,8 +21,18 @@ public class InfoService {
         this.memberDao = opt.get();
     }
 
+    @Autowired(required = false) // DateTimeFormatter 빈이 없으면 -> 호출 X
+    public void setFormatter(DateTimeFormatter formatter) {
+
+        this.formatter = formatter;
+    }
+
     public void printList() {
         List<Member> members = memberDao.getList();
-        members.forEach(System.out::println);
+        members.forEach(m -> {
+            System.out.println(m);
+            LocalDateTime regDt = m.getRegDt();
+            System.out.println(formatter.format(regDt));
+        });
     }
 }
