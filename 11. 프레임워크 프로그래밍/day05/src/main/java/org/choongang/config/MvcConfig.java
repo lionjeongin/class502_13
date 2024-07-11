d4ffd4278950c85432fb8f70676b070dee797e03
lionjeongin <cindy93489163@gmail.com>
2024-07-11 15:36:18 +0900

    Model, 정적경로

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
@@ -3,10 +3,7 @@ package org.choongang.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
@@ -19,7 +16,15 @@ public class MvcConfig implements WebMvcConfigurer {
     }
 
     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/templates/", ".jsp");
     }
+
+
 }
--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -2,12 +2,25 @@ package org.choongang.member.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MemberController {
-
+/*
     @GetMapping("/member/join")
     public String join() {
+        model.addAttribute("message", "안녕하세요.");
         return "member/join";
     }
+ */
+    @GetMapping("/member/join")
+    public ModelAndView join() {
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message", "안녕하세요.");
+
+        return mv;
+    }
+
 }
