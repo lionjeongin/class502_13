a0b893ba76c78ccb60842b7c9bbab46370d758c1
lionjeongin <cindy93489163@gmail.com>
2024-07-11 12:09:33 +0900

    day05 쿼리

---
+++ 11. 프레임워크 프로그래밍/day05/src/test/java/org/choongang/member/repositories/MemberRepositoryTest.java
@@ -0,0 +1,50 @@
+package org.choongang.member.repositories;
+
+import org.choongang.config.MvcConfig;
+import org.choongang.member.entities.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+import java.util.List;
+
+@SpringJUnitWebConfig
+@ContextConfiguration(classes = MvcConfig.class)
+public class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository repository;
+
+    @Test
+    void test1() {
+        List<Member> members = (List<Member>) repository.findAll();
+        members.forEach(System.out::println);
+    }
+
+    @Test
+    void test2() {
+        Member member = Member.builder()
+                .seq(1L)
+                .email("user01@test.org")
+                .password("12345678")
+                .userName("사용자06(수정)")
+                .build();
+
+        repository.save(member);
+    }
+
+    @Test
+    void test3() {
+        Member member = repository.findById(1L).orElse(null);
+        System.out.println(member);
+
+        repository.delete(member);
+    }
+
+    @Test
+    void test4() {
+        Member member = repository.findByEmail("user02@test.org");
+        System.out.println(member);
+    }
+}
