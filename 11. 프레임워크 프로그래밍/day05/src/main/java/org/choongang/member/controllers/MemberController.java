e2de157a3a8b3dc3bad8518600eb9c4e7582731b
lionjeongin <cindy93489163@gmail.com>
2024-07-12 11:02:16 +0900

    요청 파라미터, 커맨드 객체와 스프링 폼 연동 form

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,7 +1,6 @@
 package org.choongang.member.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,8 +11,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/member")
 public class MemberController {
 
-    //private final Logger log = LoggerFactory.getLogger(MemberController.class);
+    @GetMapping("/join")
+    public String join() {
 
+        return "member/join";
+    }
+
+    @PostMapping("/join")
+    public String joinPs(RequestJoin form) {
+
+        log.info(form.toString());
+
+        return "member/join";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+
+        return "member/login";
+    }
+
+    //private final Logger log = LoggerFactory.getLogger(MemberController.class);
+    /*
+
+
+    @PostMapping("/join")
+    public String joinPs(RequestJoin form) {
+
+       //return "redirect:/member/login"; // Location: /day05/member/login
+        return "forward:/member/login"; // 버퍼 치환
+    }
     @GetMapping("/join")
     public String join1() {
 
@@ -32,14 +59,14 @@ public class MemberController {
 
 
 
-    @PostMapping(path="/join", headers="appKey=1234", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path="/join", headers="appKey=1234", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public String joinPs(RequestJoin form) {
 
         log.info("joinPs 실행...");
 
         return "redirect:/member/login";
     }
-
+    */
     /*
     @GetMapping("/member/join")
     public ModelAndView join() {
