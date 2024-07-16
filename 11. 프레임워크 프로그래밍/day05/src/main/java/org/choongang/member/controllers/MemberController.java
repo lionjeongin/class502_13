d8f10464371968fc3e0c287cc34cf3274fe3181a
lionjeongin <cindy93489163@gmail.com>
2024-07-16 15:17:22 +0900

    예외/@ExceptionHandler/status : HTTP 상태 코드

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/controllers/MemberController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.choongang.global.exceptions.BadRequestException;
 import org.choongang.member.services.JoinService;
 import org.choongang.member.services.LoginService;
 import org.choongang.member.validators.JoinValidator;
@@ -94,8 +95,30 @@ public class MemberController {
 
         log.info(search.toString());
 
+        boolean result = false;
+        if (!result) {
+            throw new BadRequestException("예외 발생!!!");
+        }
+
+
         return "member/list";
     }
+
+    @ResponseBody
+    @GetMapping({"/info/{id}/{id2}", "/info/{id}"})
+    public void info(@PathVariable("id") String email, @PathVariable(name="id2", required = false) String email2) {
+
+        log.info("email:{}, email2:{}", email, email2);
+    }
+
+    /*
+    @ExceptionHandler(Exception.class)
+    public String errorHandler(Exception e, HttpServletRequest request, HttpServletResponse response, Model model) {
+        e.printStackTrace();
+        log.info("MemberController에서 유입");
+        return "error/common";
+    }
+    */
 
     /*
     @InitBinder
