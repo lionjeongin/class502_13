d204386e5ae19b57cb25fd0018da4a9128af9ed6
lionjeongin <cindy93489163@gmail.com>
2024-07-17 16:54:09 +0900

    7/17 실습 완료

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/member/api/controllers/ApiMemberController.java
@@ -6,6 +6,9 @@ import org.choongang.member.controllers.RequestJoin;
 import org.choongang.member.entities.Member;
 import org.choongang.member.mappers.MemberMapper;
 import org.choongang.member.services.JoinService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -22,8 +25,12 @@ public class ApiMemberController {
     private final JoinService joinService;
 
     @PostMapping // POST /api/member
-    public void join(RequestJoin form) {
-        log.info(form.toString());
+    public ResponseEntity join(@RequestBody RequestJoin form) {
+
+        joinService.process(form);
+
+        // 응답 코드 201, 출력 바디 X
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 
@@ -37,7 +44,7 @@ public class ApiMemberController {
     }
 
     @GetMapping("/list")
-    public List<Member> list() {
+    public ResponseEntity<List<Member>> list() {
         List<Member> members = IntStream.rangeClosed(1, 10)
                 .mapToObj(i -> Member.builder()
                         .email("user" + i + "@test.org")
@@ -47,7 +54,11 @@ public class ApiMemberController {
                         .build())
                 .toList();
 
-        return members;
+        HttpHeaders headers = new HttpHeaders(); // 헤더
+        headers.add("t1", "v1");
+        headers.add("t2", "v2");
+
+        return new ResponseEntity<>(members, headers, HttpStatus.OK);
     }
 
     @GetMapping(path="/test", produces = "text/html;charset=UTF-8")
