ec3c87905b8e4bf8c6a63461f396d3ae62cebf93
lionjeongin <cindy93489163@gmail.com>
2024-07-18 14:04:21 +0900

    Errors
    getFieldErrors() : 필드별 전체 에러 정보
    
    getGlobalErrors() : 커맨드 객체 자체 에러 정보 (reject(…)..)
    
    getAllErrors() : 전체 에러 정보
    
    Swagger API
    
    UriComponentsBuilder
    
    RestTemplate
    
    1. <T> ResponseEntity<T> getForEntity(...)
    2. <T> T getForObject
    3. <T> ResponseEntity<T> postForEntity
    4. <T> T postForObject
    5. <T> ResponseEntity<T> exchange(...)

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/Utils.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/global/Utils.java
@@ -1,9 +1,9 @@
 package org.choongang.global;
 
-import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
@@ -22,6 +22,8 @@ public class Utils {
 
     public Map<String, List<String>> getErrorMessages(Errors errors) {
         // FieldErrors
+
+
         Map<String, List<String>> messages = errors.getFieldErrors()
                 .stream()
                 .collect(Collectors.toMap(FieldError::getField, e -> getCodeMessages(e.getCodes())));
@@ -31,18 +33,29 @@ public class Utils {
                 .stream()
                 .flatMap(e -> getCodeMessages(e.getCodes()).stream()).toList();
 
-        messages.put("global", gMessages);
-
+        if (!gMessages.isEmpty()) {
+            messages.put("global", gMessages);
+        }
         return messages;
     }
 
 
     public List<String> getCodeMessages(String[] codes) {
+        ResourceBundleMessageSource ms = (ResourceBundleMessageSource) messageSource;
+        ms.setUseCodeAsDefaultMessage(false);
+
         List<String> messages = Arrays.stream(codes)
-                .map(c -> messageSource.getMessage(c, null, request.getLocale()))
-                .filter(s -> !StringUtils.isBlank(s))
+                .map(c -> {
+                    try {
+                        return ms.getMessage(c, null, request.getLocale());
+                    } catch (Exception e) {
+                        return "";
+                    }
+                })
+                .filter(s -> s != null && !s.isBlank())
                 .toList();
 
+        ms.setUseCodeAsDefaultMessage(true);
         return messages;
     }
 }
