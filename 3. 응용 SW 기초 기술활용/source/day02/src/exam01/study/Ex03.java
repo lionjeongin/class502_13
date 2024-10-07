1bc240d31d8366401ddac5abeb5680065fdea104
lionjeongin <cindy93489163@gmail.com>
2024-10-07 16:14:02 +0900

    for each 반복문

---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/study/Ex03.java
@@ -0,0 +1,13 @@
+package exam01.study;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        // write your code here
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        for (String day : days){
+            System.out.println(day);
+        }
+    }
+}
+}
