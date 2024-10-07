099dc66c3e93651e57092e51249001cab37e463c
lionjeongin <cindy93489163@gmail.com>
2024-10-07 16:15:19 +0900

    do-while 반복문

--- 3. 응용 SW 기초 기술활용/source/day02/src/exam01/study/Ex03.java
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/study/Ex03.java
@@ -10,4 +10,4 @@ public class Ex03 {
         }
     }
 }
-}
+
---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/study/Ex04.java
@@ -0,0 +1,15 @@
+package exam01.study;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        // write your code here
+        int i = 0;
+        int sum = 0;
+
+        do{
+            sum += (i+1);
+            i ++;
+        }while (i<10);
+        System.out.println(sum);
+    }
+}
