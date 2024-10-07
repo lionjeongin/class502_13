47a717cb4440e8622ebe9f98d53de4703a63e7a8
lionjeongin <cindy93489163@gmail.com>
2024-10-07 16:07:15 +0900

    조건문

---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/study/Main.java
@@ -0,0 +1,25 @@
+package exam01.study;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String score = sc.next();
+
+        switch(score){
+            case "A":
+                System.out.println("A등급입니다.");
+                break;
+            case "B":
+                System.out.println("B등급입니다.");
+                break;
+            case "C":
+                System.out.println("C등급입니다.");
+                break;
+            default:
+                System.out.println("D등급 이하 입니다.");
+                break;
+        }
+    }
+}
